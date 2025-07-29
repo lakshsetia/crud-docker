b624347d32aab969cb968426481dbcb125928d8a
Laksh Setia <lakshsetia30@gmail.com>
2025-07-29 22:15:10 +0530

    updated storage.go postgresql.go

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/lakshsetia/crud-docker/internal/config"
+	"github.com/lakshsetia/crud-docker/internal/types"
 	_ "github.com/lib/pq"
 )
 
@@ -36,4 +37,48 @@ func New(config *config.Config) (*Postgresql, error) {
 	return &Postgresql{
 		DB: db,
 	}, nil
+}
+func (p *Postgresql) GetUsers() ([]types.User, error) {
+	rows, err := p.DB.Query("SELECT id, name, email, age FROM users")
+	if err != nil {
+		return nil, err
+	}
+	users := make([]types.User, 0)
+	for rows.Next() {
+		var user types.User
+		if err = rows.Scan(&user.ID, &user.Name, &user.Email, &user.Age); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
+func (p *Postgresql) CreateUser(user types.User) error {
+	if _, err := p.DB.Exec("INSERT INTO users (name, email, age) VALUES ($1, $2, $3)", user.Name, user.Email, user.Age); err != nil {
+		return err
+	}
+	return nil
+}
+func (p *Postgresql) GetUserById(id int) (types.User, error) {
+	var user types.User
+	err := p.DB.QueryRow("SELECT id, name, email, age FROM users WHERE id=$1", id).Scan(&user.ID, &user.Name, &user.Email, &user.Age)
+	if err == sql.ErrNoRows {
+		return user, fmt.Errorf("user not found with id=%v", id)
+	}
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+func (p *Postgresql) UpdateUserById(id int, newUser types.User) error {
+	if _, err := p.DB.Exec("UPDATE users SET name=$1, email=$2, age=$3 WHERE id=$4", newUser.Name, newUser.Email, newUser.Age, id); err != nil {
+		return err
+	}
+	return nil
+}
+func (p *Postgresql) DeleteUserById(id int) error {
+	if _, err := p.DB.Exec("DELETE FROM users WHERE id=$1", id); err != nil {
+		return err
+	}
+	return nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,4 +1,11 @@
 package storage
 
+import "github.com/lakshsetia/crud-docker/internal/types"
+
 type Storage interface {
+	GetUsers() ([]types.User, error)
+	CreateUser(user types.User) error
+	GetUserById(id int) (types.User, error)
+	UpdateUserById(id int, newUser types.User) error
+	DeleteUserById(id int) error
 }
